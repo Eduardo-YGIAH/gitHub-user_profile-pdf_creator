f201f30da83c460102b25acb0d4999a02c89d5fe
Eduardo Neto <edu.neto@icloud.com>
2019-12-02 19:44:18 +0000

    Project completion

--- index.js
+++ index.js
@@ -4,26 +4,28 @@ const puppeteer = require("puppeteer");
 const fs = require("fs-extra");
 const hbs = require("handlebars");
 const path = require("path");
+require("dotenv").config();
+
+const url = process.env.GITHUB_API;
 
 askQuestions([
   "Type a GitHub username: ",
   "What is your favourite colour? "
 ]).then(answers => {
-  let user = answers[0];
-  let backgroundColor = answers[1];
-  let url = `https://api.github.com/users/${user}`;
+  const user = answers[0];
+  const backgroundColor = answers[1];
+
   const gitHubIcon =
     "https://res.cloudinary.com/ygiah/image/upload/v1574460349/bootcamp/github-alt-brands_1.svg";
   const locationIcon =
     "https://res.cloudinary.com/ygiah/image/upload/v1574460349/bootcamp/location-arrow-solid_1.svg";
   const blogIcon =
     "https://res.cloudinary.com/ygiah/image/upload/v1574460350/bootcamp/rss-solid_1.svg";
-  const userInfo = fetch(url);
+  const userInfo = fetch(`${url}/users/${user}`);
 
   userInfo
     .then(response => response.json())
     .then(res => {
-      console.log(res);
       const data = {
         name: res.name,
         avatar: res.avatar_url,
@@ -50,40 +52,44 @@ askQuestions([
       stars
         .then(response => response.json())
         .then(pro => {
-          console.log(pro[0].stargazers_count);
+          const userStars = {
+            stars: pro[0].stargazers_count
+          };
+
+          data.stars = userStars.stars;
+
+          const compile = async function(templateName, data) {
+            const filePath = path.join(
+              process.cwd(),
+              "template",
+              `${templateName}.hbs`
+            );
+            const html = await fs.readFile(filePath, "utf-8");
+            return hbs.compile(html)(data);
+          };
+
+          (async function() {
+            try {
+              const browser = await puppeteer.launch();
+              const page = await browser.newPage();
+
+              const content = await compile("profile-template", data);
+              await page.setContent(content);
+
+              await page.emulateMedia("screen");
+              await page.pdf({
+                path: "profile.pdf",
+                format: "A4",
+                printBackground: true
+              });
+
+              console.log("DONE");
+              await browser.close();
+              process.exit();
+            } catch (error) {
+              console.log("opps, I messed up again!", error);
+            }
+          })();
         });
-
-      const compile = async function(templateName, data) {
-        const filePath = path.join(
-          process.cwd(),
-          "template",
-          `${templateName}.hbs`
-        );
-        const html = await fs.readFile(filePath, "utf-8");
-        return hbs.compile(html)(data);
-      };
-
-      (async function() {
-        try {
-          const browser = await puppeteer.launch();
-          const page = await browser.newPage();
-
-          const content = await compile("profile-template", data);
-          await page.setContent(content);
-
-          await page.emulateMedia("screen");
-          await page.pdf({
-            path: "profile.pdf",
-            format: "A4",
-            printBackground: true
-          });
-
-          console.log("DONE");
-          await browser.close();
-          process.exit();
-        } catch (error) {
-          console.log("opps, I messed up again!", error);
-        }
-      })();
     });
 });
